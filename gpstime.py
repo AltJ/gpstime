a3557d3eb54801148477118178266e63979dd0f9
AltJ <alt@altj.org>
2013-05-21 22:52:30 -0600

    initial commit of code

---
+++ gpstime.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import time
+from gps import *
+
+print 'Attempting to access GPS time...'
+
+try:
+	gpsd = gps(mode=WATCH_ENABLE)
+except:
+	print 'No GPS connection present. TIME NOT SET.'
+	sys.exit()
+
+while True:
+	gpsd.next()
+	if gpsd.utc != None and gpsd.utc != '':
+		gpstime = gpsd.utc[0:4] + gpsd.utc[5:7] + gpsd.utc[8:10] + ' ' + gpsd.utc[11:19]
+		print 'Setting system time to GPS time...'
+		os.system('sudo date --set="%s"' % gpstime)
+		print 'System time set.'
+		sys.exit()
+	time.sleep(1)
